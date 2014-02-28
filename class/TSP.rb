1b3538e2e5b9284f2e22d7758df00b20131403a3
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-02-28 21:03:40 +0100

    Se ha añadido el metodo signature, el cual devuelve cual seria la clave que se usa en la tabla de hash para acceder a la distancia entre dos ciudades

--- class/TSP.rb
+++ class/TSP.rb
@@ -4,29 +4,22 @@ class TSP
 
 	# Lee el fichero de BD y lo carga en memoria
 	def leer_intancia(path_db)
-		raise TypeError, "path_db debe de ser un string" unless path_db.class.name == "String"
-		@lista_ciudades = Array.new
-		@caminos = Hash.new
-
-		File.open(path_db, "r") do |f|
-			# La primera lectura corresponde al numero total de ciudades
-			# que en nuestro caso no no interesa saber a priori, ya que
-			# no tenemos los mismo problemas que un programa en C, así
-			# que la descartamos.
-			#
-			# Aun con todo, como la matriz es simetrica, usaremos dicho
-			# valor para solo tratar de leer la mitad de la matriz
-			max_ciudades = f.gets.to_i
-
-
-			f.each do |linea|
-			end
-		end
 	end
 
 	# Constructor de la clase TSP. Recibe como argumento
 	# el fichero del cual debe de leer la matriz de distancias
 	def initialize(path_db)
 		self.leer_intancia(path_db)
+	end
+
+	# Devuelve la llave de hash asociada a dos ciuades que se
+	# pasen como argumento
+	def obtener_signature(ciudad_a, ciudad_b)
+		raise TypeError, "ciudad_a debe de ser un String" unless ciudad_a.class.name == "String"
+		raise TypeError, "ciudad_b debe de ser un String" unless ciudad_b.class.name == "String"
+
+		signature = Array.new
+		signature << ciudad_a << ciudad_b
+		signature.sort!
 	end
 end
