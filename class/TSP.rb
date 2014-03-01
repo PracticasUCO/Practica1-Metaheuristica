45b1f51de12253ca56eae4d43b6b8c1011db614c
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-01 16:18:50 +0100

    Redefinido el metodo TSP#leer_intancia

--- class/TSP.rb
+++ class/TSP.rb
@@ -6,29 +6,27 @@ class TSP
 	def leer_intancia(path_db)
 		raise TypeError, "path_db debe de ser un String" unless path_db.class.name == "String"
 
-		@caminos = Hash.new
+		@caminos = Array.new
 
 		File.open(path_db, "r") do |f|
 			# La primera linea nos da información acerca del numero de ciudades que hay
 			# almacenadas en el fichero
 			#
-			# Aunque a priori el numero no no importa, ya que en Ruby no tenemos que
-			# que hacer una reserva de memoria anticipada, si nos sirve para no leer toda
-			# la base de datos, teniendo en cuenta que las distancias son simetricas
-			max_ciudades = f.gets.to_i
+			# Dicho valor no nos importa a priori, por lo que no lo guardamos
+			f.gets
 
 			# Ahora podemos ir realizando una lectura completa del resto de la base de datos
 			#
 			# Uso el enumerador with_index para parar la ejecucion una vez que se haya leido
 			# la mitad de la base de datos
 			f.each_with_index do |linea, index|
-				break unless index < max_ciudades / 2
-				linea = linea.chomp
-				ciudades = linea.split(/ +/)
+				fila_costes = linea.chomp.split(/ +/)
+				
+				#Convertimos la fila de string a flotante
+				fila_costes.each_with_index {|coste, index| fila_costes[index] = coste.to_f}
 
-				ciudades.each_with_index do |coste|
-
-				end
+				#Añadimos la fila completa al array
+				@caminos << fila_costes
 			end
 		end
 	end
@@ -52,3 +50,5 @@ class TSP
 
 	private :obtener_signature
 end
+
+a = TSP.new("/home/gowikel/Practicas con Git/Practica1-Metaheuristica/instancias/TSP/p01.txt")
