bfdace9fbeb7459cd2d5226d9a6a441e12f381b2
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-03 13:22:47 +0100

    Se ha borrado los metodos BasicMMDP#busqueda_local y BasicMMDP#busqueda_global

--- class/BasicMMDP.rb
+++ class/BasicMMDP.rb
@@ -74,58 +74,6 @@ class BasicMMDP
 		end
 	end
 
-	# Realiza una busca local para tratar de mejorar lo maximo posible el
-	# vector solucion
-	# 
-	# Recibe como parametros el array con la soluciones escogidas y
-	#
-	# Devuelve un vector solucion optimizado
-	def busqueda_local(solucion)
-		raise TypeError, "El parametro solucion debe ser un Array" unless solucion.kind_of? Array
-
-		alternativa = solucion.dup
-		nodos_lista = lista_nodos().dup
-		coste_actual = obtener_suma_costes(solucion)
-
-		alternativa.each_with_index do |origen, index|
-			break if index > solution_nodes / @punto_ruptura
-			alternativa.each do |destino|
-				next if origen == destino
-
-				nueva_alternativa = alternativa.dup
-				nueva_alternativa.delete(destino)
-				
-				nodos_lista.each do |nodo_alternativo|
-					next if nodo_alternativo == origen
-					next if nodo_alternativo == destino
-					next if nueva_alternativa.include? nodo_alternativo
-
-					nueva_alternativa << nodo_alternativo
-
-					coste_alternativa = obtener_suma_costes(nueva_alternativa)
-
-					if coste_alternativa > coste_actual
-						coste_actual = coste_alternativa
-						alternativa = nueva_alternativa.dup
-					end
-
-					nueva_alternativa.delete(nodo_alternativo)
-				end
-			end
-		end
-
-		return alternativa, coste_actual
-	end
-
-	# Realiza una busqueda global para tratar de obtener un
-	# vector con la maxima distancia entre nodos posible
-	def busqueda_global
-		solucion = lista_nodos().dup.sample(solution_nodes)
-		coste_actual = obtener_suma_costes(solucion)
-
-		return solucion, coste_actual
-	end
-
 	# Devuelve la solucion optima del problema
 	def solucion_optima
 		coste = -1.0
@@ -218,5 +166,4 @@ class BasicMMDP
 
 	# Definicion de los metodos privados de la clase
 	private :obtener_coste_entre, :obtener_suma_costes
-	protected :busqueda_global, :busqueda_local
 end
