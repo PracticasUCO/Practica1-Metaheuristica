5930c8ac5ae6251c1d5e19b5617876e101c8b56c
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-04 19:47:51 +0100

    Se ha añadido el atributo grafo, que guardara los caminos posibles del grafo en cuestion

--- class/BasicCWP.rb
+++ class/BasicCWP.rb
@@ -1,5 +1,12 @@
 #! /usr/bin/env ruby -w
 
 class BasicCWP
+	# El atributo grafo guarda una tabla de hash con cada
+	# uno de los nodos del grafo. La tabla usara como
+	# llave el nodo del grafo, y el valor que devolvera sera
+	# un vector con todas las posiciones a las que se puede
+	# ir a partir de dicho nodo
+	attr_reader :grafo
+	
 	
 end
