ad7ac7b11d62aaa09490f9e6010b1b454631d39f
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-06 20:03:51 +0100

    Añadidas más comprobaciones a la distancia

--- test/test_capacited_p_hub_node.rb
+++ test/test_capacited_p_hub_node.rb
@@ -73,6 +73,13 @@ class TestCapacitedPHubNode < Test::Unit::TestCase
 		d2 = @clienteB.distancia(@concentradorA)
 		assert_equal(4, d1)
 		assert_equal(2, d2)
+		
+		coordenadaX, coordenadaY = @clientes[0]
+		@clientes.each do |cliente|
+			coordenadaX_cliente, coordenadaY_cliente = cliente.coordenadas
+			distancia = Math.sqrt((coordenadaX - coordenadaX_cliente) ** 2 + (coordenadaY - coordenadaY_cliente) ** 2)
+			assert_equal(distancia, @clientes[0].distancia(cliente))
+		end
 	end
 	
 	def test_demanda
