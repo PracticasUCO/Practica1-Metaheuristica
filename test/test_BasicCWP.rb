e5d132994fdf1282a7171abc7a73804afc73176c
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-09 18:20:50 +0100

    Se ha añadido un test para asegurarse de que la generacion de la solucion aleatoria es constante (en complejidad temporal), esta vez se ha realizado el test contra todos los ficheros de instancia que hay en la carpeta intancias/CWP

--- test/test_BasicCWP.rb
+++ test/test_BasicCWP.rb
@@ -58,3 +58,19 @@ class TestBasicCWP < MiniTest::Test
 		assert_operator(soluciones.length, :>, repeticiones*7/10)
 	end
 end
+
+class Test_BasicCWPBenchmark < MiniTest::Benchmark
+	def self.bench_range()
+		path = `find . | grep instancias/CWP/`.split(/\n/)
+		return MiniTest::Benchmark.bench_linear(0, path.length - 1, 1)
+	end
+	
+	def bench_solucion_aleatoria
+		path = `find . | grep instancias/CWP/`.split(/\n/)
+		
+		assert_performance_constant 0.9999 do |index| # n is a range value
+			b = BasicCWP.new(path[index])
+			b.generar_solucion_aleatoria
+		end
+	end
+end
