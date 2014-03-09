70512f1194ccda9e9d1dc35156b2ab5b05325fdc
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-09 18:36:50 +0100

    Añadido un benchmark para comprobar la complejida temporal de la generacion de la solucion aleatoria. En este caso, debido a que tengo pocas intancias del problema, en lugar de asegurase al 0.9999 lo he relajado a 0.99

--- test/test_BasicTSP.rb
+++ test/test_BasicTSP.rb
@@ -44,3 +44,19 @@ class TestBasicTSP < MiniTest::Test
 		assert_operator(soluciones.length, :>, repeticiones*8/10)
 	end
 end
+
+class BenchBasicTSP < MiniTest::Benchmark
+	def self.bench_range
+		paths = `find . | grep 'instancias/TSP/'`.split(/\n/)
+		MiniTest::Benchmark.bench_linear(0, paths.length - 1, 1)
+	end
+	
+	def bench_solucion_aleatoria
+		paths = `find . | grep 'instancias/TSP/'`.split(/\n/)
+		
+		assert_performance_constant 0.99 do |index|
+			b = BasicTSP.new(paths[index])
+			b.generar_solucion_aleatoria
+		end
+	end
+end
