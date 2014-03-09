801f0840813779c920d9ee11c0f79ce14fe9503a
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-09 18:28:51 +0100

    Añadido un Benchmark para asegurarse de que el tiempo de generacion de la solucion aleatoria es constante. Se usan todos los ficheros de instancia proporcionados para comprobar tal fin

--- test/test_BasicMMDP.rb
+++ test/test_BasicMMDP.rb
@@ -34,3 +34,19 @@ class TestBasicMMDP < MiniTest::Test
 		assert_operator(sol.length, :>=, repeticiones*8/10)
 	end
 end
+
+class BenchBasicMMDP < MiniTest::Benchmark
+	def self.bench_range
+		paths = `find . | grep 'instancias/MMDP/'`.split(/\n/)
+		MiniTest::Benchmark.bench_linear(0, paths.length - 1, 1)
+	end
+	
+	def bench_solucion_aleatoria
+		paths = `find . | grep 'instancias/MMDP/'`.split(/\n/)
+		
+		assert_performance_constant 0.9999 do |index|
+			b = BasicMMDP.new(paths[index])
+			b.generar_solucion_aleatoria
+		end
+	end
+end
