22f9818e43f9bf56f4b38737bdebd50db256a7f4
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-09 18:58:32 +0100

    Creado el metodo BasicPHub#leer_instancia

--- class/BasicPHub.rb
+++ class/BasicPHub.rb
@@ -15,4 +15,34 @@ class BasicPHub
 	# que hay en la base de datos. Estos nodos puede estar ya conectados
 	# o no, dependiendo de si ha elegido conectarlos o no
 	attr_reader :nodos
+	
+	# Constructor de la clase BasicPHub, recibe como paremetro un
+	# String que indica de donde leer la base de datos.
+	def initialize(path_db)
+		leer_instancia(path_db)
+	end
+	
+	# Lee una instancia de la Base de datos y almacena todos los nodos
+	# en su interior
+	def leer_instancia(path_db)
+		raise TypeError, "path_db debe de ser un string" unless path_db.kind_of? String
+		
+		@nodos.clear
+		
+		File.open(path_db, "r") do |file|
+			# De la primera linea se lee el numero de nodos concentradores y
+			# la capacidad por concentrador
+			*, numero_concentradores, capacidad_concentrador = file.gets.split(/ +/)
+			@numero_concentradores = numero_concentradores.to_i
+			@capacidad_concentrador = capacidad_concentrador.to_f
+			
+			# Ahora se lee el resto del fichero
+			file.each do |linea|
+				linea = linea.chomp
+				*, coorX, coorY, demanda = linea.split(/ +/)
+				nodo = CapacitedPHubNode.new(coordenadas: [coorX, coorY], demanda: demanda, capacidad_servicio: capacidad_concentrador)
+				@nodos << nodo
+			end
+		end
+	end
 end
