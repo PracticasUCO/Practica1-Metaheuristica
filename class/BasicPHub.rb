e77a6bbc1e3edfb6d0ec0f3f9129f727ce5d2a5b
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-09 18:46:45 +0100

    Se añadido el atributo nodos

--- class/BasicPHub.rb
+++ class/BasicPHub.rb
@@ -11,5 +11,8 @@ los nodos del problema.
 Además de esto, debe dar un solución basica generada de forma aleatoria
 =end
 class BasicPHub
-	
+	# nodos guarda una representacion vectorial de todos los nodos
+	# que hay en la base de datos. Estos nodos puede estar ya conectados
+	# o no, dependiendo de si ha elegido conectarlos o no
+	attr_reader :nodos
 end
