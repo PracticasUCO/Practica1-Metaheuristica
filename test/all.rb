2368163482fafdb897332b92b91494118e8038d6
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-10 23:56:25 +0100

    Se ha añadido el fichero all.rb el cual ejecutara todos los test disponibles

---
+++ test/all.rb
@@ -0,0 +1,5 @@
+require_relative 'test_BasicPHub'
+require_relative 'test_BasicCWP'
+require_relative 'test_BasicMMDP'
+require_relative 'test_BasicTSP'
+require_relative 'test_capacited_p_hub_node'
