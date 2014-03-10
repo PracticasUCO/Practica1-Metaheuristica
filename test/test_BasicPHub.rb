7763eacb69b9c3fc1993539e8ae3b6a14c38e65b
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-10 23:20:48 +0100

    Añadido un test para comprobar el numero de concentradores de la solucion

--- test/test_BasicPHub.rb
+++ test/test_BasicPHub.rb
@@ -13,4 +13,17 @@ class TestBasicPHub < MiniTest::Test
 		assert_equal(5, @t.numero_concentradores, "Debe de haber 5 concentradores en la base de datos")
 		assert_equal(120, @t.capacidad_concentrador)
 	end
+	
+	def test_numero_concentradores_solucion
+		numero_concentradores = 0
+		solucion, coste = @t.generar_solucion_aleatoria
+		
+		solucion.each do |nodo|
+			numero_concentradores += 1 if nodo.tipo.eql? :concentrador
+		end
+		
+		if coste != Float::INFINITY
+			assert_equal(5, numero_concentradores, "El numero de concentradores de la solucion es incorrecto")
+		end
+	end
 end
