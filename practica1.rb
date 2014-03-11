28df6eee3fb40f6264eacb419cd0e9d5b107ba29
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-11 18:36:06 +0100

    Añadido el parametro loops que indica el numero de iteraciones a dar

--- practica1.rb
+++ practica1.rb
@@ -25,7 +25,8 @@ opt = Getopt::Long.getopts(
 	["--help", "-h", Getopt::BOOLEAN],
 	["--type", "-p", Getopt::REQUIRED],
 	["--instance", "-f", Getopt::REQUIRED],
-	["--seed", "-s", Getopt::REQUIRED]
+	["--seed", "-s", Getopt::REQUIRED],
+	["--loops", "-l", Getopt::REQUIRED]
 	)
 rescue Getopt::Long::Error => e
 	puts "Una o varias de las opciones introduccidas es incorrecta."
@@ -38,20 +39,22 @@ if opt["help"]
 	puts "# ruby nombreScript [opciones]"
 	puts ""
 	puts "Las opciones de este programa son:"
-	puts "\t--help o -h: Muestra esta ayuda"
+	puts "\t --help o -h: Muestra esta ayuda"
 	puts
-	puts "\t--type o -p: Indica el tipo de problema a tratar, es un"
-	puts "parametro que debe de introduccirse siempre. Puede contener"
-	puts "los siguientes valores:"
-	puts "\t\t-MMDP: Resolución de Max Min Diversity Problem"
-	puts "\t\t-CWP: Resolución de problema CutWidth Problem"
-	puts "\t\t-TSP: Resolución de Travelling Salesman Problem"
-	puts "\t\t-CPH: Resolución de Capacited P-Hub Problem"
+	puts "\t --type o -p: Indica el tipo de problema a tratar, es un"
+	puts "\t parametro que debe de introduccirse siempre. Puede contener"
+	puts "\t los siguientes valores:"
+	puts "\t\t -MMDP: Resolución de Max Min Diversity Problem"
+	puts "\t\t -CWP: Resolución de problema CutWidth Problem"
+	puts "\t\t -TSP: Resolución de Travelling Salesman Problem"
+	puts "\t\t -CPH: Resolución de Capacited P-Hub Problem"
 	puts
-	puts "\t--instance o -f: Indica de donde leer la instancia. Debe de introduccirse"
-	puts "siempre este parametro"
+	puts "\t --instance o -f: Indica de donde leer la instancia. Debe de introduccirse"
+	puts "\t siempre este parametro"
 	puts
-	puts "\t--seed o -s: Indica la semilla del programa"
+	puts "\t --seed o -s: Indica la semilla del programa"
+	puts "\t --loops o -l: Indica el numero de repeticiones del programa."
+	puts "\t si no se especifica, tiene un valor por defecto de 1000"
 	exit
 end
 
@@ -99,4 +102,19 @@ elsif opt["type"] == "TSP"
 	rescue RuntimeError => e
 		mostrar_error_fichero_incorrecto opt["instance"]
 	end
+elsif opt["type"] == "CPH"
+	begin
+		problem = BasicPHub.new(opt["instance"])
+	rescue Errno::ENOENT => e
+		mostrar_error_fichero_no_encontrado opt["instance"]
+	rescue TypeError => e
+		mostrar_error_fichero_incorrecto opt["instance"]
+	rescue RuntimeError => e
+		mostrar_error_fichero_incorrecto opt["instance"]
+	end
+else
+	puts "Argumento no reconocido: #{opt["type"]}"
+	exit
+end
+	
 end
