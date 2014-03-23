d3a455d6cb335c88f9d41bb648b540d0b9e1e0c0
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-23 21:36:51 +0100

    Añadido el inicializador de la clase de ruby

---
+++ lib/ES/ES/c_es.c
@@ -0,0 +1,15 @@
+#include "c_es.h"
+
+void Init_ES()
+{
+	module_es = rb_define_module("ES");
+	class_es = rb_define_class_under(module_es, "ES", rb_cObject);
+	rb_define_method(class_es, "initialize", method_initiallize, 1);
+	rb_define_method(class_es, "temperatura", method_temperatura, 0);
+	rb_define_method(class_es, "probabilidad", method_probabilidad, 0);
+	rb_define_method(class_es, "enfriar", method_enfriar, 0);
+	rb_define_method(class_es, "valor_inicio", method_valor_inicio, 0);
+	rb_define_method(class_es, "tipo", method_tipo, 0);
+	rb_define_method(class_es, "coeficiente", method_coeficiente, 0);
+	rb_define_method(class_es, "reset", method_reset, 0);
+}
