99ce0b8a87a9b2f59c0abc237a8b2fff76863416
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-23 22:09:16 +0100

    He movido la funcion void Init_ES() al fondo para que sea concordante con los prototipos que he visto de la implementación en Ruby

--- lib/ES/ES/c_es.c
+++ lib/ES/ES/c_es.c
@@ -1,18 +1,5 @@
 #include "c_es.h"
 
-void Init_ES()
-{
-	module_es = rb_define_module("ES");
-	class_es = rb_define_class_under(module_es, "ES", rb_cObject);
-	rb_define_method(class_es, "initialize", method_initialize, 1);
-	rb_define_method(class_es, "temperatura", method_temperatura, 0);
-	rb_define_method(class_es, "probabilidad", method_probabilidad, 0);
-	rb_define_method(class_es, "enfriar", method_enfriar, 0);
-	rb_define_method(class_es, "valor_inicio", method_valor_inicio, 0);
-	rb_define_method(class_es, "tipo", method_tipo, 0);
-	rb_define_method(class_es, "coeficiente", method_coeficiente, 0);
-	rb_define_method(class_es, "reset", method_reset, 0);
-}
 
 VALUE method_temperatura(VALUE self)
 {
@@ -79,4 +66,18 @@ VALUE method_reset(VALUE self)
 	VALUE valorInicial = rb_iv_get(self, "valor_inicio");
 	rb_iv_set(self, "temperatura", valorInicial);
 	return Qnil;
+}
+
+void Init_ES()
+{
+	module_es = rb_define_module("ES");
+	class_es = rb_define_class_under(module_es, "ES", rb_cObject);
+	rb_define_method(class_es, "initialize", method_initialize, 1);
+	rb_define_method(class_es, "temperatura", method_temperatura, 0);
+	rb_define_method(class_es, "probabilidad", method_probabilidad, 0);
+	rb_define_method(class_es, "enfriar", method_enfriar, 0);
+	rb_define_method(class_es, "valor_inicio", method_valor_inicio, 0);
+	rb_define_method(class_es, "tipo", method_tipo, 0);
+	rb_define_method(class_es, "coeficiente", method_coeficiente, 0);
+	rb_define_method(class_es, "reset", method_reset, 0);
 }
