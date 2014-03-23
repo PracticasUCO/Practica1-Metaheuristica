8677a7acf7e640ef80f83d3633fe47c915cf81e2
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-23 21:29:09 +0100

    Iniciada la implementación de la clase ES que es la encargada de realizar el Enfriamiento Simulado. La idea es implementar la clase en C de forma integra.

---
+++ lib/ES/ES/c_es.h
@@ -0,0 +1,65 @@
+#ifndef __C_ENFRIAMIENTO_SIMULADO__
+#define __C_ENFRIAMIENTO_SIMULADO__
+
+#include <ruby.h>
+
+// Constructor de la clase ES
+void Init_ES();
+
+// Clase ES
+VALUE class_es = Qnil;
+
+/*
+Constructor de la clase, recibe como parametros una tabla de hash con
+los siguientes atributos:
+- valor_inicio: Indica el valor de inicio de la temperatura, debe de ser
+  numerico y solo puede oscilar entre 0-1 (ambos inclusive)
+- tipo: Indica el tipo de función que se usara para disminuir la temperatura
+  Actualmente solo puede ser :geometrica
+- coeficiente: Indica el valor del coeficiente usado en la función de disminución
+  de la temperatura
+*/
+VALUE method_initialize;
+
+/*
+Este metodo devuelve la temperatura actual del algoritmo
+*/
+VALUE method_temperatura;
+
+/*
+Este metodo devuelve un valor booleano True o False de forma aleatoria.
+La probabilidad de que devuelva True sera mayor contra más alta sea
+la temperatura.
+*/
+VALUE method_probabilidad;
+
+/*
+Disminuye la temperatura interna de la clase de manera que haya menor
+probabilidad de conseguir un valor True.
+
+La funcion de disminución de la temperatura viene determinada por el
+tipo y por el coeficiente de cambio determinado en la clase
+*/
+VALUE method_enfriar;
+
+/*
+Devuelve el valor de inicio de la temperatura de la clase
+*/
+VALUE method_valor_inicio;
+
+/*
+Devuelve el tipo de funcion de disminución de la temperatura,
+actualmente este valor solo puede ser :geometrica
+*/
+VALUE method_tipo;
+
+/*
+Devuelve el coeficiente de cambio usado en la funcion de temperatura
+*/
+VALUE method_coeficiente;
+
+/*
+Restaura la temperatura a su punto inicial
+*/
+VALUE method_reset;
+#endif
---
+++ lib/ES/ES/extconf.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+# encoding: utf-8
+
+# Loads mkmf which is used to make makefiles for Ruby extensions
+require 'mkmf'
+
+# Give it a name
+extension_name = 'ES'
+
+# The destination
+dir_config(extension_name)
+
+# Do the work
+create_makefile(extension_name)  
