caedf14d525a9d797f3626d33f0fdebe64eee405
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-24 21:22:50 +0100

    Se ha añadido un test para comprobar que el constructor es correcto

--- test/MMDP/test_MMDP.rb
+++ test/MMDP/test_MMDP.rb
@@ -5,6 +5,16 @@ require 'minitest/autorun'
 
 describe MMDP do
 	before do
-		@t = MMDP.new("instancias/P2/MMDP/GKD-Ia_41_n15_m9.txt")
+		@t = MMDP::MMDP.new("instancias/P2/MMDP/GKD-Ia_41_n15_m9.txt")
+	end
+
+	describe "Cuando se crea la clase con la instancia GKD-Ia_41_n15_m9" do
+		it "Debe de tener 9 nodos solucion" do
+			@t.solution_nodes.must_equal 9
+		end
+
+		it "Debe de haber leido un total de 15 nodos" do
+			@t.total_nodes.must_equal 15
+		end
 	end
 end
