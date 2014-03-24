442303fbb36cea78e4c139d01ba887319c6f3f9c
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-24 13:53:46 +0100

    Se ha añadido el atributo aceptacion que indica el grado de probabilidad de aceptar una solución distinta a la actual

--- lib/ES/ES/ES.rb
+++ lib/ES/ES/ES.rb
@@ -12,15 +12,17 @@ module ES
 	class ES
 		# Constructor de la clase, recibe como parametros:
 		#	- valor_inicio: Indica el valor de inicio de la temperatura, debe de ser
-  		#		numerico y solo puede oscilar entre 0-1 (ambos inclusive)
+  	#		numerico y solo puede oscilar entre 0-1 (ambos inclusive)
 		#	- tipo: Indica el tipo de función que se usara para disminuir la temperatura
-  		#		Actualmente solo puede ser :geometrica
+  	#		Actualmente solo puede ser :geometrica
 		#	- coeficiente: Indica el valor del coeficiente usado en la función de disminución
-  		#		de la temperatura
-  		def initialize(valor_inicio: 1, tipo: :geometrica, coeficiente: 0.85)
+  	#		de la temperatura
+    # - aceptacion: probabilidad de aceptacion en tanto por uno de una solucion distinta
+  		def initialize(valor_inicio: 1, tipo: :geometrica, coeficiente: 0.85, aceptacion: 1)
   			raise TypeError, "valor_inicio debe de ser numerico" unless valor_inicio.kind_of? Numeric
   			raise TypeError, "tipo de valer :geometrica" unless tipo.eql? :geometrica
   			raise TypeError, "coeficiente debe de ser un valor numerico" unless coeficiente.kind_of? Numeric
+        raise TypeError, "aceptacion debe de ser un valor numerico" unless aceptacion.kind_of? Numeric
 
   			if valor_inicio > 1 or valor_inicio < 0
   				raise TypeError, "valor_inicio debe de estar entre 0 y 1 (ambos inclusive)"
@@ -34,6 +36,7 @@ module ES
   			@tipo = tipo
   			@coeficiente = coeficiente
   			@temperatura = valor_inicio
+        @aceptacion = aceptacion
   		end
 	end
 end
