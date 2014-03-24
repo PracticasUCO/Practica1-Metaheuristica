1cb5dc6739719d9502389b86ea43fbccf581dc1b
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-24 21:27:31 +0100

    Añadido más descripciones al fichero de test

--- test/MMDP/test_MMDP.rb
+++ test/MMDP/test_MMDP.rb
@@ -1,11 +1,13 @@
 #! /usr/bin/env ruby
 
 require_relative '../../lib/MMDP/MMDP/MMDP'
+require_relative '../../lib/MMDP/BasicMMDP/BasicMMDP'
 require 'minitest/autorun'
 
 describe MMDP do
 	before do
 		@t = MMDP::MMDP.new("instancias/P2/MMDP/GKD-Ia_41_n15_m9.txt")
+		@b = MMDP::BasicMMDP.new("instancias/P2/MMDP/GKD-Ia_41_n15_m9.txt")
 	end
 
 	describe "Cuando se crea la clase con la instancia GKD-Ia_41_n15_m9" do
@@ -15,6 +17,18 @@ describe MMDP do
 
 		it "Debe de haber leido un total de 15 nodos" do
 			@t.total_nodes.must_equal 15
+		end
+
+		it "Debe de tener de tratar de obtener el minimo de la funcion objetivo" do
+			@t.clasificador.must_equal :minima
+		end
+	end
+
+	describe "Cuando se halla una solucion mediante busqueda local." do
+		it "Debe de tener una longitud de 9" do
+		end
+
+		it "El coste debe de ser mayor o igual que la obtenida con BasicMMDP" do
 		end
 	end
 end
