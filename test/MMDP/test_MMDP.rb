08cb77ba492dfd366e0e9d592293d2885f9ec560
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-26 14:01:09 +0100

    Se ha modificado el test para que coja una instancia más pesada de la base de datos

--- test/MMDP/test_MMDP.rb
+++ test/MMDP/test_MMDP.rb
@@ -6,17 +6,17 @@ require 'minitest/autorun'
 
 describe MMDP do
 	before do
-		@t = MMDP::MMDP.new("instancias/P2/MMDP/GKD-Ia_41_n15_m9.txt")
-		@b = MMDP::BasicMMDP.new("instancias/P2/MMDP/GKD-Ia_41_n15_m9.txt")
+		@t = MMDP::MMDP.new("instancias/P2/MMDP/GKD-Ia_71_n30_m24.txt")
+		@b = MMDP::BasicMMDP.new("instancias/P2/MMDP/GKD-Ia_71_n30_m24.txt")
 	end
 
-	describe "Cuando se crea la clase con la instancia GKD-Ia_41_n15_m9" do
-		it "Debe de tener 9 nodos solucion" do
-			@t.solution_nodes.must_equal 9
+	describe "Cuando se crea la clase con la instancia GKD-Ia_71_n30_m24" do
+		it "Debe de tener 24 nodos solucion" do
+			@t.solution_nodes.must_equal 24
 		end
 
 		it "Debe de haber leido un total de 15 nodos" do
-			@t.total_nodes.must_equal 15
+			@t.total_nodes.must_equal 30
 		end
 
 		it "Debe de tener de tratar de obtener el minimo de la funcion objetivo" do
@@ -25,14 +25,14 @@ describe MMDP do
 	end
 
 	describe "Cuando se halla una solucion mediante busqueda local." do
-		it "Debe de tener una longitud de 9" do
+		it "Debe de tener una longitud de 24" do
 			solucion, * = @t.generar_solucion_busqueda_local
-			solucion.length.must_equal 9
+			solucion.length.must_equal 24
 		end
 
 		it "El coste medio debe de ser mayor o igual que la obtenida con BasicMMDP" do
 
-			repeticiones = 30
+			repeticiones = 5
 			suma_best_improvement = 0
 			suma_aleatorio = 0
 
@@ -50,16 +50,13 @@ describe MMDP do
 	end
 
 	describe "En las soluciones de busqueda local por Best Improvement" do
-		it "Ninguna solucion puede superar el coste de 300 ya que estamos tratando de diversidad minima" do
-			repeticiones = 30
-			suma_costes = 0
+		it "Ninguna solucion puede superar el coste de 200 ya que estamos tratando de diversidad minima" do
+			repeticiones = 5
 			repeticiones.times do
 				*, coste = @t.generar_solucion_busqueda_local
 
-				suma_costes += coste
+				coste.must_be :<, 200
 			end
-
-			suma_costes.must_be :<, 400*repeticiones
 		end
 
 		it "El coste de la solucion debe de ser mayor que cero" do
