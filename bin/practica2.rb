132b43f3e36378ae02dda436e037e1bf33a7e25d
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-27 19:42:36 +0100

    Añadida la carga de la clase TSP

--- bin/practica2.rb
+++ bin/practica2.rb
@@ -85,4 +85,14 @@ if opt["type"] == "MMDP"
 	rescue RuntimeError => e
 		mostrar_error_fichero_incorrecto(opt['instance'])
 	end
+elsif opt["type"] == "TSP"
+	begin
+		problem = TSP::TSP.new(opt["instance"])
+	rescue Errno::ENOENT => e
+		mostrar_error_fichero_no_encontrado(opt['instance'])
+	rescue TypeError => e
+		mostrar_error_fichero_incorrecto(opt['instance'])
+	rescue RuntimeError => e
+		mostrar_error_fichero_incorrecto(opt['instance'])
+	end
 end
