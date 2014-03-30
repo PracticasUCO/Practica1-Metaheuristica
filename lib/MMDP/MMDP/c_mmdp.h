b971d229daf4b7e9f85b0c771980a32cdb5d4137
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-03-30 19:33:06 +0200

    Añadidas las funciones más basicas

--- lib/MMDP/MMDP/c_mmdp.h
+++ lib/MMDP/MMDP/c_mmdp.h
@@ -2,6 +2,13 @@
 #define __C_MMDP__
 
 #include <ruby.h>
+#include "../BasicMMDP/c_basic_mmdp.h"
+
+// Inicializador de la clase
+void Init_c_mmdp();
+
+// Espacio para la clase MMDP
+VALUE class_mmdp;
 
 
 #endif
