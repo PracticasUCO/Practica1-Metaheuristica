67cf249328a316cd0ec2184224c3a292a7283577
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-02 22:31:46 +0200

    Borrado el metodo ES::ES#valor_inicio de la carga de clase

--- lib/ES/ES/c_es.c
+++ lib/ES/ES/c_es.c
@@ -98,7 +98,6 @@ void Init_c_es()
 	rb_define_method(class_es, "temperatura", method_temperatura, 0);
 	rb_define_method(class_es, "probabilidad", method_probabilidad, 0);
 	rb_define_method(class_es, "enfriar", method_enfriar, 0);
-	rb_define_method(class_es, "valor_inicio", method_valor_inicio, 0);
 	rb_define_method(class_es, "tipo", method_tipo, 0);
 	rb_define_method(class_es, "coeficiente", method_coeficiente, 0);
 	rb_define_method(class_es, "reset", method_reset, 0);
