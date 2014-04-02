e9c8cbd318e5693509dbe9b7f8f58d731dca2a1b
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-02 11:02:26 +0200

    Reescrito el metodo MMDP#obtener_diferencia_soluciones para que sea mas rapido y de una mejor solucion

--- lib/MMDP/MMDP/c_mmdp.c
+++ lib/MMDP/MMDP/c_mmdp.c
@@ -36,8 +36,6 @@ Los parametros de esta funcion son:
 VALUE method_obtener_diferencia_soluciones(VALUE self, VALUE solucion_actual, VALUE coste_actual, 
 														VALUE nodo_eliminar, VALUE new_node)
 {
-	VALUE particion_eliminar;
-	VALUE coste_nodo_eliminado;
 	VALUE coste_nuevo_nodo;
 	VALUE vector_auxiliar;
 	VALUE coste_final;
@@ -46,17 +44,22 @@ VALUE method_obtener_diferencia_soluciones(VALUE self, VALUE solucion_actual, VA
 		// solucion_actual debe ser un array --> Ok
 		// coste_actual debe ser un numero --> Not yet
 		// nodo_eliminar debe de estar en la solucion --> Not yet
-		// new_ndde debe de estar en la lista de nodos de la solucion --> Not yet
+		// new_node debe de estar en la lista de nodos de la solucion --> Not yet
 	solucion_actual = rb_check_array_type(solucion_actual);
 	// Algoritmo
-	particion_eliminar = rb_ary_new();
-	rb_ary_push(particion_eliminar, nodo_eliminar);
+	
+	vector_auxiliar = rb_ary_dup(solucion_actual);
+	rb_ary_delete(vector_auxiliar, nodo_eliminar);
+	coste_nuevo_nodo = method_diversidad_minima_parcial(self, vector_auxiliar, new_node);
 
-	vector_auxiliar = method_ary_diff(solucion_actual, particion_eliminar);
-	coste_nuevo_nodo = method_merge_diversidad_minima(self, vector_auxiliar, new_node);
-	coste_nodo_eliminado = method_merge_diversidad_minima(self, vector_auxiliar, nodo_eliminar);
-
-	coste_final = DBL2NUM(NUM2DBL(coste_actual) - NUM2DBL(coste_nodo_eliminado) + NUM2DBL(coste_nuevo_nodo));
+	if(NUM2DBL(coste_nuevo_nodo) < NUM2DBL(coste_actual))
+	{
+		coste_final = coste_nuevo_nodo;
+	}
+	else
+	{
+		coste_final = coste_actual;
+	}
 
 	return coste_final;
 }
