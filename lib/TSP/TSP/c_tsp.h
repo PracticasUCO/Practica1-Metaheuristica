f00a7d738f4c6f70b155804363aa0eef394e4d7e
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-05 16:15:50 +0200

    Añadidos simbolos globales
    Se han añadido los siguientes simbolos como globales:
    - :first_improvement
    - :best_improvement
    - :enfriamiento_simulado

--- lib/TSP/TSP/c_tsp.h
+++ lib/TSP/TSP/c_tsp.h
@@ -1,3 +1,11 @@
+#ifndef __LOCAL_SEARCH_SYMBOLS
+#define __LOCAL_SEARCH_SYMBOLS
+// Global Syms
+VALUE symbol_first_improvement = ID2SYM(rb_intern("first_improvemnt"));
+VALUE symbol_best_improvement = ID2SYM(rb_intern("best_improvement"));
+VALUE symbol_enfriamiento_simulado = ID2SYM(rb_intern("enfriamiento_simulado"));
+#endif
+
 #ifndef __TSP_C__
 #define __TSP_C__
 
@@ -11,4 +19,6 @@ void Init_c_tsp();
 VALUE method_tsp_mejora_solucion(VALUE self, VALUE solucion_a, VALUE costeA, VALUE nodo_a, VALUE nodo_b);
 VALUE method_tsp_busqueda_local_first_improvement(VALUE self, VALUE solucion, VALUE coste_solucion, VALUE limite);
 VALUE method_tsp_busqueda_local_best_improvement(VALUE self, VALUE solucion, VALUE coste_solucion, VALUE limite);
+VALUE method_tsp_busqueda_local(VALUE self);
+
 #endif
