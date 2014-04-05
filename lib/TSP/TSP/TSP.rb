80299dedb835e76b2576189830f5250f03ba739c
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-05 19:15:45 +0200

    Implementado el metodo busqueda local

--- lib/TSP/TSP/TSP.rb
+++ lib/TSP/TSP/TSP.rb
@@ -9,5 +9,25 @@ module TSP
 			raise "path must be a string" unless path.kind_of? String
 			super
 		end
+
+		def busqueda_local(tipo)
+			raise "tipo must be a symbol" unless tipo.kind_of? Symbol
+
+			unless tipo == :first_improvement or tipo == :best_improvement or tipo == :enfriamiento_simulado
+				raise "Valor de tipo desconocido: #{tipo}."
+			end
+
+			solucion, coste = generar_solucion_aleatoria()
+
+			if tipo == :first_improvement
+				solucion, coste = busqueda_local_first_improvement(solucion, coste, 0);
+			elsif tipo == :best_improvement
+				# Not implemented yet
+			else
+				# Not implemented yet
+			end
+
+			return solucion, coste
+		end
 	end
 end
