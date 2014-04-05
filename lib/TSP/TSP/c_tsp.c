2bb5bbce12d3b1a1f096a41d3d2c75915c9cf30c
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-05 19:30:50 +0200

    Añadida una funcion para poder visualizar el vector solucion

--- lib/TSP/TSP/c_tsp.c
+++ lib/TSP/TSP/c_tsp.c
@@ -1,6 +1,17 @@
 #include "c_tsp.h"
 #include "../BasicTSP/c_basic_tsp.h"
 #include "../BasicTSP/c_basic_tsp.c"
+#include <stdio.h>
+
+void show_vector(VALUE ary)
+{
+	int i;
+	for(i = 0; i < RARRAY_LEN(ary); i++)
+	{
+		fprintf(stderr, "%d - ", NUM2INT(rb_ary_entry(ary, i)));
+	}
+	fprintf(stderr, "\n");
+}
 
 /*
 Este metodo realiza el intercambio de dos nodos en una posicion concreta
