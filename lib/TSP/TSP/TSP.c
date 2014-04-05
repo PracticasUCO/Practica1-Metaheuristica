a8839e0c01f37e1cbb8b942829c35c5e3b2aa2a5
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-05 13:26:35 +0200

    Se han añadido los ficheros basicos para la clase TSP
    En un principio he pensado en tratar de hacer la clase TSP enteramente
    en C, con la API de Ruby

---
+++ lib/TSP/TSP/TSP.c
@@ -0,0 +1,1 @@
+#include "TSP.h"
---
+++ lib/TSP/TSP/TSP.h
@@ -0,0 +1,10 @@
+#ifndef __TSP_C__
+#define __TSP_C__
+
+#include <ruby.h>
+#include "../BasicTSP/c_basic_tsp.c" //Esto habra que cambiarlo
+
+VALUE class_tsp;
+void Init_TSP();
+
+#endif
