e4cc7c02f9f28fc7f64e5f4214ad784b94fa55fd
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-15 22:51:15 +0200

    Cambiado el nombre del ultimo argumento para los metodos operador_seleccion_torneo y operador_selección_ruleta

--- lib/PHUB/PHUB/c_phub.h
+++ lib/PHUB/PHUB/c_phub.h
@@ -9,8 +9,8 @@ VALUE class_phub = Qnil;
 //Metodos
 VALUE random_number(VALUE self);
 VALUE separar_nodos(VALUE self, VALUE solucion);
-VALUE operador_seleccion_torneo(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_selecciones);
-VALUE operador_seleccion_ruleta(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_selecciones);
+VALUE operador_seleccion_torneo(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_elementos);
+VALUE operador_seleccion_ruleta(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_elementos);
 VALUE operador_seleccion(VALUE self, VALUE id_seleccion, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_selecciones);
 VALUE operador_cruce(VALUE self, VALUE solucion_a, VALUE solucion_b);
 VALUE operador_mutacion(VALUE self, VALUE solucion);
