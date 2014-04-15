c45074b860e2a11a48cc69d75e78c5b711d1b2e9
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-15 22:38:48 +0200

    Añadida las banderas extras para la optimización del código

--- lib/PHUB/PHUB/extconf.rb
+++ lib/PHUB/PHUB/extconf.rb
@@ -4,5 +4,7 @@ require 'mkmf'
 
 extension_name = "c_phub"
 
+$CFLAGS="-fbounds-check -fdefault-inline -ffast-math -ffloat-store -fforce-addr -ffunction-cse -finline"
+
 dir_config(extension_name)
 create_makefile(extension_name)
