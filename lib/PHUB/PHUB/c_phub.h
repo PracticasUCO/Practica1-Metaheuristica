f2b914ffa7680e0183ca0c248748dcb6d5edee0a
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-15 22:14:27 +0200

    Añadidos los prototipos iniciales

--- lib/PHUB/PHUB/c_phub.h
+++ lib/PHUB/PHUB/c_phub.h
@@ -2,3 +2,13 @@
 #include "../phub.h"
 #include "../CapacitedPHubNode/c_basic_phub_node.h"
 #include "../BasicPHub/c_basic_phub.h"
+
+// Clase PHUB
+VALUE class_phub = Qnil;
+
+//Metodos
+VALUE operador_seleccion_torneo(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_selecciones);
+VALUE operador_seleccion_ruleta(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_selecciones);
+VALUE operador_seleccion(VALUE self, VALUE id_seleccion, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_selecciones);
+VALUE operador_cruce(VALUE self, VALUE solucion_a, VALUE solucion_b);
+VALUE operador_mutacion(VALUE self, VALUE solucion);
