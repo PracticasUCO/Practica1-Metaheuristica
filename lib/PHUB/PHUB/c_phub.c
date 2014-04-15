e266090e1c142ac3aa8f51b9b4eaa379038051f8
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-15 22:37:32 +0200

    Implementado el metodo PHUB#separar_nodos

--- lib/PHUB/PHUB/c_phub.c
+++ lib/PHUB/PHUB/c_phub.c
@@ -10,3 +10,41 @@ VALUE random_number(VALUE self)
 	double random_number = drand48();
 	return DBL2NUM(random_number);
 }
+
+/*
+Separa los nodos concentradores de una solución de los nodos
+que actúan como clientes. Se devuelven en dos vectores el primero
+con los nodos concentradores y el segundo con los nodos cliente.
+*/
+VALUE separar_nodos(VALUE self, VALUE solucion)
+{
+	VALUE concentradores;
+	VALUE clientes;
+	VALUE empaquetado;
+	VALUE sym_concentrador = ID2SYM(rb_intern("concentrador"));
+	int i; //Auxiliar
+	
+	concentradores = rb_ary_new();
+	clientes = rb_ary_new();
+	empaquetado = rb_ary_new();
+	
+	for(i = 0; i < RARRAY_LEN(solucion); i++)
+	{
+		VALUE item = rb_ary_entry(solucion, i);
+		VALUE tipo = rb_funcall(item, rb_intern("tipo"), 0);
+		
+		if(tipo == sym_concentrador)
+		{
+			rb_ary_push(concentradores, item);
+		}
+		else
+		{
+			rb_ary_push(clientes, item);
+		}
+	}
+	
+	rb_ary_push(empaquetado, concentradores);
+	rb_ary_push(empaquetado, clientes);
+	
+	return empaquetado;
+}
