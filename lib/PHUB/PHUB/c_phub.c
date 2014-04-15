3f1577c289749a4d52f2160216f1a715ffa0d423
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-15 22:27:33 +0200

    Implementado el metodo PHUB#random_number

--- lib/PHUB/PHUB/c_phub.c
+++ lib/PHUB/PHUB/c_phub.c
@@ -1,2 +1,12 @@
 #include <ruby.h>
+#include <stdlib.h>
 #include "c_phub.h"
+
+/*
+Genera un valor aleatorio entre 0 y 1
+*/
+VALUE random_number(VALUE self)
+{
+	double random_number = drand48();
+	return DBL2NUM(random_number);
+}
