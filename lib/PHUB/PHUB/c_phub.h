889febd396b2114eb84542e0c2a3a87af1fd90a5
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-16 13:02:51 +0200

    Se ha añadido de nuevo el prototipo para PHUB#seleccion

--- lib/PHUB/PHUB/c_phub.h
+++ lib/PHUB/PHUB/c_phub.h
@@ -12,5 +12,6 @@ VALUE phub_separar_nodos(VALUE self, VALUE solucion);
 VALUE phub_operador_seleccion_torneo(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_elementos);
 VALUE phub_operador_seleccion_torneo_injusto(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_elementos);
 VALUE phub_operador_seleccion_ruleta(VALUE self, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_elementos);
+VALUE phub_operador_seleccion(VALUE self, VALUE sym_seleccion, VALUE lista_soluciones, VALUE fitness_soluciones, VALUE n_elementos);
 VALUE phub_operador_cruce(VALUE self, VALUE solucion_a, VALUE solucion_b);
 VALUE phub_operador_mutacion(VALUE self, VALUE solucion);
