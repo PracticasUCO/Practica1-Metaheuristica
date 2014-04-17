12f7f6f78332b6c62aef6fa5a417dee8c5064765
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-17 20:30:28 +0200

    Añadida la descripción para PHUB#random_number

--- test/PHUB/test_phub.rb
+++ test/PHUB/test_phub.rb
@@ -3,7 +3,7 @@
 require_relative '../../lib/PHUB/PHUB/PHUB'
 require 'minitest/autorun'
 
-# Esta clase servira para probar los metodos privados
+# Esta clase servirá para probar los métodos privados
 # de la clase PHUB
 class PHUBPrivate < PHUB::PHUB
 	public :random_number, :separar_nodos, :torneo, :torneo_injusto, :ruleta, :seleccion
@@ -12,5 +12,16 @@ end
 describe PHUBPrivate do
 	before do
 		@t = PHUBPrivate.new("instancias/P1/CPH/phub_50_5_1.txt")
+	end
+	
+	describe "Cuando se llama a random_number" do
+		it "Debe de devover un número entre 0-1" do
+			300.times do
+				numero_generado = @t.random_number
+				
+				numero_generado.must_be :<=, 1
+				numero_generado.must_be :>=, 0
+			end
+		end
 	end
 end
