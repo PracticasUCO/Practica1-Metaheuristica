62b4c0f40b6c337b9ed65b2647b42f950d927dc2
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-19 14:20:11 +0200

    Se ha implementado el método PHUB#get_connections

--- lib/PHUB/PHUB/c_phub.c
+++ lib/PHUB/PHUB/c_phub.c
@@ -382,6 +382,46 @@ VALUE phub_operador_seleccion(VALUE self, VALUE sym_seleccion, VALUE lista_soluc
 	}
 }
 
+/*
+Devuelve una tabla de hash que indica los nodos a los que estaba conectado
+cada concentrador de la solución
+*/
+VALUE phub_get_conexiones(VALUE self, VALUE solucion)
+{
+	VALUE table = rb_hash_new();
+	long i;
+	
+	Check_Type(solucion, T_ARRAY);
+	
+	for(i = 0; i < RARRAY_LEN(solucion); i++)
+	{
+		VALUE nodo = rb_ary_entry(solucion, i);
+		
+		if(rb_funcall(nodo, rb_intern("tipo"), 0) == ID2SYM(rb_intern("cliente")))
+		{
+			continue;
+		}
+		else
+		{
+			VALUE connected = rb_iv_get(nodo, "@connected");
+			
+			//Comprobar si es necesario
+			if(TYPE(connected) == T_ARRAY)
+			{
+				connected = rb_ary_dup(connected);
+			}
+			else
+			{
+				connected = rb_ary_new();
+			}
+			
+			rb_hash_aset(table, nodo, connected);
+		}
+	}
+	
+	return table;
+}
+
 void Init_c_phub()
 {
 	phub_module = rb_define_module("PHUB");
