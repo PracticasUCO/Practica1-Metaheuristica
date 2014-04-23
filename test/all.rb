d081d041125eceb7b045ac94951d3efcbb26fc22
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-04-23 16:25:34 +0200

    Actualizado para que ejecute todos los test existentes automaticamente

--- test/all.rb
+++ test/all.rb
@@ -1,13 +1,7 @@
 #! /usr/bin/env ruby
 # encoding: utf-8
-require_relative 'CWP/bench_BasicCWP'
-require_relative 'MMDP/bench_BasicMMDP'
-require_relative 'PHUB/bench_BasicPHub'
-require_relative 'TSP/bench_BasicTSP'
-require_relative 'PHUB/test_BasicPHub'
-require_relative 'CWP/test_BasicCWP'
-require_relative 'MMDP/test_BasicMMDP'
-require_relative 'TSP/test_BasicTSP'
-require_relative 'PHUB/test_capacited_p_hub_node'
-require_relative 'ES/test_es'
-require_relative 'MMDP/test_MMDP'
+require_relative 'CWP/all'
+require_relative 'MMDP/all'
+require_relative 'PHUB/all'
+require_relative 'TSP/all'
+require_relative 'ES/all'
