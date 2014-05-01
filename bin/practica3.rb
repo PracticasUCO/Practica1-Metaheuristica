6d671d93afd013d5a79b5907a18a81648c6b103c
Pedro José Piquero Plaza <gowikel@gmail.com>
2014-05-01 13:45:54 +0200

    Añadida la descripción de --help y reconocimiento de la semilla usada.
    Ahora el programa puede mostrar la ayuda por pantalla y muestra también
    la semilla que esta usando el algoritmo para generar los resultados

--- bin/practica3.rb
+++ bin/practica3.rb
@@ -21,8 +21,39 @@ begin
 	["--help", "-h", Getopt::BOOLEAN],
 	["--dir", "-d", Getopt::REQUIRED],
 	["--threads", "-t", Getopt::REQUIRED],
-	["--save", "-s", Getopt::REQUIRED])
+	["--save", "-s", Getopt::REQUIRED],
+	["--show", nil, Getopt::BOOLEAN],
+	["--hide-seed", nil, Getopt::BOOLEAN])
 rescue Getopt::Long::Error => e
 	puts "Uno o varios de los parametros opcionales es incorrecto."
 	puts "Compruebe con --help la entrada antes de continuar"
+end
+
+random_seed = srand(123)
+srand(random_seed)
+
+unless opt["hide-seed"]
+	puts "\nSeed: #{random_seed}\n\n"
+end
+
+if opt["help"]	
+	puts "Este programa busca una solución al problema del PHUB mediante el uso"
+	puts "de algoritmos evolutivos."
+	puts "Concretamente la busqueda se realiza mediante el algoritmo evolutivo"
+	puts "estacionario y el algoritmo evolutivo generacional."
+	puts ""
+	puts "Ejecute este programa como ./practica3.rb [OPCIONES]"
+	puts "Las opciones de este programa son:"
+	puts "\t --help -h: Muestra esta ayuda."
+	puts "\t --dir -d: Indica la carpeta donde se encuentran todos los ficheros"
+	puts "\t de instancias del PHUB"
+	puts "\t --threads -t: Indica el número de hilos que se usaran para procesar"
+	puts "\t cada instancia. Se recomienda utilizar un número igual al número de"
+	puts "\t nucleos del ordenador donde se este ejecutando."
+	puts "\t --save -s: Indica el lugar donde guardar el coste de la solución de"
+	puts "\t cada fichero procesado."
+	puts "\t --show: Cuando se especifica se muestra por pantalla la solución"
+	puts "\t generada y su coste."
+	puts "\t --hide-seed: No muestra la semilla generada"
+	exit(0)
 end
